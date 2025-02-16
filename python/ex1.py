536bdc04cfc1fc7218bba651161c82ac05ce630f
Yeshwant Singh <yeshwant.singh@DDN.UPES.AC.IN>
2025-02-16 11:29:00 +0530

    Intro to Python

--- python/ex1.py
+++ python/ex1.py
@@ -1,16 +1,13 @@
-x = 2 # integer
-y = 3.3 # float (real valued number)
+name = "Yeshwant Singh" # textual data (string)
+date = '16/02/2025' # string
+day = 16    # integer
+month = 2
+year = 2025
+temperature = 25.6 # real number (float)
 
-msg = 'Hello World' # string (text)
+is_it_sunday = True # boolean (True/False)
 
-z = True # boolean (True or False)
-
-print(z)
-
-z = False
-
-print(z)
-
-print(msg)
-
-print(x + y)
+# data type
+# numeric data type   : integer, float
+# text data type      : string
+# boolean data type   : True/False
