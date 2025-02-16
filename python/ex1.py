4e63ac0ccbaf1326ff139403f802a495471113ea
Yeshwant Singh <yeshwant.singh@DDN.UPES.AC.IN>
2025-02-16 11:27:02 +0530

    Intro to Python

---
+++ python/ex1.py
@@ -0,0 +1,16 @@
+x = 2 # integer
+y = 3.3 # float (real valued number)
+
+msg = 'Hello World' # string (text)
+
+z = True # boolean (True or False)
+
+print(z)
+
+z = False
+
+print(z)
+
+print(msg)
+
+print(x + y)
---
+++ python/ex2.py
@@ -0,0 +1,75 @@
+# age = 10
+
+# print(age)          # function
+
+# age_after_10_years = age + 10
+
+# age_10_year_ago = age - 10
+
+# print(age_after_10_years)
+# print(age_10_year_ago)
+
+number_1 = 10
+number_2 = 20
+number_3 = 30
+
+# Conditional Statements
+check = not ((number_1 <= number_2) or (number_2 <= number_3))
+
+
+print(check) 
+
+
+
+
+# Artihmetic Operators
+# +, -, *, /, %, **, //
+
+# Comparison Operators
+# ==, !=, >, <, >=, <=
+
+# Logical Operators
+# and, or, not
+
+
+# AND
+# False and False = False
+# False and True = False
+# True and False = False
+# True and True = True
+
+# OR
+# False or False = False
+# False or True = True
+# True or False = True
+# True or True = True
+
+# NOT  ## unary operator    
+# not False = True
+# not True = False
+
+# Assignment Operators
+# =, +=, -=, *=, /=, %=, **=, //=
+
+# age = 10
+# age += 10 # age = age + 10
+# age -= 10 # age = age - 10
+# age *= 10 # age = age * 10
+# age /= 10 # age = age / 10
+# age %= 10 # age = age % 10
+# age **= 10 # age = age ** 10
+# age //= 10 # age = age // 10
+
+# Bitwise Operators
+# &, |, ~, ^, <<, >>
+
+# Membership Operators
+# in, not in
+
+# 10 in [10, 20, 30]
+
+# Identity Operators
+# is, is not
+
+# check is age
+
