eb081e8aecaf085425931cf72a681f3d0ab06ee6
Yeshwant Singh <yeshwant.singh@DDN.UPES.AC.IN>
2025-03-02 11:30:03 +0530

    removed old folder structure

--- python/ex5.py
+++
@@ -1,9 +0,0 @@
-import numpy
-import matplotlib
-import pandas
-import seaborn
-
-print('numpy:', numpy.__version__)
-print('matplotlib:', matplotlib.__version__)
-print('pandas:', pandas.__version__)
-print('seaborn:', seaborn.__version__)
